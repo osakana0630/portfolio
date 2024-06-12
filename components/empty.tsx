25db07001fbe18b71b7ac48248d4370b14020b11
osakana0630 <osakana0630@gmail.com>
2024-06-12 23:18:53 +0900

    リファクタリング

--- components/empty.tsx
+++ components/empty.tsx
@@ -4,15 +4,16 @@ import { Rocket } from "lucide-react";
 import { Button } from "@/components/ui/button";
 import { useRouter } from "next/navigation";
 
-export function Empty() {
+type Props = {
+  title?: string;
+};
+export function Empty({ title = "データがありません。" }: Props) {
   const router = useRouter();
   return (
     <div className="flex flex-col items-center justify-center gap-6 border rounded-lg bg-muted p-12">
       <div className="text-center flex flex-col items-center gap-2">
         <Rocket className="size-24 lg:size-28" />
-        <h2 className="text-lg lg:text-xl font-semibold">
-          データがありません。
-        </h2>
+        <h2 className="text-lg lg:text-xl font-semibold">{title}</h2>
       </div>
 
       <Button onClick={() => router.back()} className="w-fit">
