469635cc8133e5f293fe6bdcfb90312aeac87cfe
osakana0630 <osakana0630@gmail.com>
2025-07-10 02:36:01 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import { ThemeProvider } from "@/components/theme-provider";
+import { ThemeProvider } from "next-themes";
 import "./globals.css";
 import { Header } from "@/components/layouts/header";
 import { cn } from "@/lib/utils";
