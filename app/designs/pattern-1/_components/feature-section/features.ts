45cfaa74c05c501690d41daf27115b30ff158d87
osakana0630 <osakana0630@gmail.com>
2025-10-19 22:23:49 +0900

    set basePath

--- app/designs/pattern-1/_components/feature-section/features.ts
+++ app/designs/pattern-1/_components/feature-section/features.ts
@@ -1,24 +1,29 @@
+import nextConfig from "../../../../../next.config";
+
+const BASE_PATH = nextConfig.basePath || "";
+
+
 export const features = [
   {
     id: 1,
     title: "Fully Customizable",
     description:
       "A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem ",
-    icon: "/pattern1/feature1.svg",
+    icon: `${BASE_PATH}/pattern1/feature1.svg`,
   },
   {
     id: 2,
     title: "Fully Customizable",
     description:
       "A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem ",
-    icon: "/pattern1/feature1.svg",
+    icon: `${BASE_PATH}/pattern1/feature1.svg`,
   },
   {
     id: 3,
     title: "Fully Customizable",
     description:
       "A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem ",
-    icon: "/pattern1/feature1.svg",
+    icon: `${BASE_PATH}/pattern1/feature1.svg`,
     // 強調
     isEmphasized: true,
   },
@@ -27,20 +32,20 @@ export const features = [
     title: "Fully Customizable",
     description:
       "A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem ",
-    icon: "/pattern1/feature1.svg",
+    icon: `${BASE_PATH}/pattern1/feature1.svg`,
   },
   {
     id: 5,
     title: "Fully Customizable",
     description:
       "A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem ",
-    icon: "/pattern1/feature1.svg",
+    icon: `${BASE_PATH}/pattern1/feature1.svg`,
   },
   {
     id: 6,
     title: "Fully Customizable",
     description:
       "A good design is not only aesthetically pleasing, but also functional. It should be able to solve the problem ",
-    icon: "/pattern1/feature1.svg",
+    icon: `${BASE_PATH}/pattern1/feature1.svg`,
   },
 ];
